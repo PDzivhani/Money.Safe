017a0d477db2e73a8d60d222771cbc920a599594
PDzivhani <dzivhanip0505@gmail.com>
2024-08-01 13:23:35 +0200

    backend begin

---
+++ Backend/MoneySafe/src/main/java/com/Fullstack/MoneySafe/MoneySafeApplication.java
@@ -0,0 +1,13 @@
+package com.Fullstack.MoneySafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneySafeApplication.class, args);
+	}
+
+}
