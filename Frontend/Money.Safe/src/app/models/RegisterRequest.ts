c1e0c418f20e4ebb92513e05faee89cedb910249
PDzivhani <dzivhanip0505@gmail.com>
2024-08-01 16:29:45 +0200

    models

---
+++ Frontend/Money.Safe/src/app/models/RegisterRequest.ts
@@ -0,0 +1,8 @@
+export interface RegisterRequest {
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }
